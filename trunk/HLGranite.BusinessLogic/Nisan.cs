a6542161d57458dfd7c2cb2a938fb7a3ce84078a
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2008-10-28 10:07:31 +0000

    First add into svn repo.

---
+++ trunk/HLGranite.BusinessLogic/Nisan.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HLGranite.BusinessLogic
+{
+    public class Nisan
+    {
+        public Nisan()
+        {
+        }
+    }//end class
+}//end namespace since 20080215tys
