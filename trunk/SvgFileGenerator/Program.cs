774c3d82647eea7e99e47d188510e531f5149b6f
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2010-08-25 07:45:55 +0000

    added SvgFileGenarator project which automate generating svg file based on template tile_arabic.svg and tile_andalus.svg.

---
+++ trunk/SvgFileGenerator/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.IO;
+using System.Linq;
+using System.Configuration;
+
+namespace SvgFileGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            XmlSerializer serializer = new XmlSerializer(typeof(nisan));
+            FileStream stream = new FileStream("nisan.xml", FileMode.Open);
+            nisan nisans = serializer.Deserialize(stream) as nisan;
+            stream.Flush();
+            stream.Close();
+            System.Diagnostics.Debug.WriteLine(nisans.Items.Length);
+
+            List<nisanOrder> orders = new List<nisanOrder>();
+            List<nisanPurchase> purchases = new List<nisanPurchase>();
+            foreach (object obj in nisans.Items)
+            {
+                if (obj.GetType() == typeof(nisanOrder))
+                    orders.Add(obj as nisanOrder);
+                if (obj.GetType() == typeof(nisanPurchase))
+                    purchases.Add(obj as nisanPurchase);
+            }
+            System.Diagnostics.Debug.WriteLine("Total order: " + orders.Count);
+            System.Diagnostics.Debug.WriteLine("Total purchase: " + purchases.Count);
+
+
+            //get undelivered order
+            int length = ConfigurationManager.AppSettings.Keys.Count;
+            for (int i = 0; i < length; i++)
+            {
+                ConfigurationManager.AppSettings.Keys[i].ToString();
+                string item = ConfigurationManager.AppSettings.Keys[i].ToString();
+                string[] lookupFiles = ConfigurationManager.AppSettings.GetValues(i);
+                if (lookupFiles.Length < 1) continue;
+                List<nisanOrder> undelivered = orders
+                    .Where(f => f.delivered.Length == 0 && f.item == item).ToList<nisanOrder>();
+                Console.WriteLine(string.Format("There are {0}:{1} pending", item, undelivered.Count));
+                foreach (nisanOrder order in undelivered)
+                {
+                    Console.WriteLine("Writing " + order.name + ".svg...");
+                    SvgWriter writer = new SvgWriter(order, lookupFiles[0]);
+                    writer.Save();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Complete");
+
+            //wait for user input
+            Console.Read();
+        }
+
+        /*static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            XmlSerializer serializer = new XmlSerializer(typeof(nisan));
+            FileStream stream = new FileStream("nisan.xml", FileMode.Open);
+            nisan nisans = serializer.Deserialize(stream) as nisan;
+            stream.Flush();
+            stream.Close();
+            System.Diagnostics.Debug.WriteLine(nisans.Items.Length);
+
+            List<nisanOrder> orders = new List<nisanOrder>();
+            List<nisanPurchase> purchases = new List<nisanPurchase>();
+            foreach (object obj in nisans.Items)
+            {
+                if (obj.GetType() == typeof(nisanOrder))
+                    orders.Add(obj as nisanOrder);
+                if (obj.GetType() == typeof(nisanPurchase))
+                    purchases.Add(obj as nisanPurchase);
+            }
+            System.Diagnostics.Debug.WriteLine("Total order: " + orders.Count);
+            System.Diagnostics.Debug.WriteLine("Total purchase: " + purchases.Count);
+
+
+            //get undelivered order
+            //Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //config.AppSettings.Settings["dataPath"].Value
+            int length = ConfigurationManager.AppSettings.Keys.Count;
+            for (int i = 0; i < length; i++)
+            {
+                ConfigurationManager.AppSettings.Keys[i].ToString();
+                string item = ConfigurationManager.AppSettings.Keys[i].ToString();
+                string[] lookupFiles = ConfigurationManager.AppSettings.GetValues(i);
+                if (lookupFiles.Length < 1) continue;
+                List<nisanOrder> undelivered = orders
+                    .Where(f => f.delivered.Length == 0 && f.item == item).ToList<nisanOrder>();
+                System.Diagnostics.Debug.WriteLine(string.Format("There are {0}:{1} pending", item, undelivered.Count));
+                foreach (nisanOrder order in undelivered)
+                {
+                    var elements = GetXMLElements(lookupFiles[0], "text");
+                    System.Diagnostics.Debug.Write("found text element: " + elements.Count());
+                    foreach (XElement e in elements)
+                    {
+                        XAttribute attribute = e.Attribute(XName.Get("id"));
+                        string message = string.Format("Name: {0} Value:{1}", e.Name, e.Value);
+                        System.Diagnostics.Debug.WriteLine(message);
+                    }
+                }
+            }
+        }*/
+        /// <summary>
+        /// Returns element of an XML file.
+        /// </summary>
+        /// <param name="inputUrl"></param>
+        /// <param name="elementName"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Alternative code:
+        /// <example>
+        /// <code>
+        /// XmlReader reader = XmlReader.Create(lookupFiles[0]);
+        /// while (reader.Read())
+        /// {
+        ///    switch (reader.NodeType)
+        ///    {
+        ///        case XmlNodeType.Element:
+        ///            string message = string.Format("Name: {0} Value:{1}", reader.Name, reader.Value);
+        ///            System.Diagnostics.Debug.WriteLine(message);
+        ///            break;
+        ///        case XmlNodeType.Attribute:
+        ///            break;
+        ///    }
+        /// }
+        /// </code>
+        /// </example>
+        /// </remarks>
+        /// <seealso>http://stackoverflow.com/questions/2441673/reading-xml-with-xmlreader-in-c</seealso>
+        /// <seealso>http://support.microsoft.com/kb/307548</seealso>
+        static IEnumerable<XElement> GetXMLElements(string inputUrl, string elementName)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ProhibitDtd = false;
+            using (XmlReader reader = XmlReader.Create(inputUrl, settings))
+            {
+                reader.MoveToContent();
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name == elementName)
+                        {
+                            XElement e = XNode.ReadFrom(reader) as XElement;
+                            if (null != e) yield return e;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ trunk/SvgFileGenerator/SvgWriter.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SvgFileGenerator
+{
+    /// <summary>
+    /// An svg writer class.
+    /// </summary>
+    /// <remarks>
+    /// 2010-08-25: Use a StreamReader to find and replace the text context then TextWriter write a file.
+    /// In this version, must ensure maintain the svg file according to predefined template.
+    /// future: Use XElement, Xml serializer method for better performance.
+    /// http://blogs.msdn.com/b/xmlteam/archive/2007/03/24/streaming-with-linq-to-xml-part-2.aspx
+    /// </remarks>
+    public class SvgWriter
+    {
+        #region Fields
+        private string templatePath;
+        private nisanOrder order;
+        private StreamReader reader;
+        private TextWriter writer;
+        private Action action;
+        private string[] muslimMonths = new string[12]{
+               "محرّم","صفر","ربيع الاول","ربيع الاخير","جمادالاول","جمادالاخير",
+               "رجب","شعبان","رمضان","شوال","ذوالقعده","ذوالحجه"
+        };
+        #endregion
+
+        /// <summary>
+        /// Recommended constructor.
+        /// </summary>
+        /// <param name="order">Order.</param>
+        /// <param name="templatePath">Lookup template file.</param>
+        public SvgWriter(nisanOrder order, string templatePath)
+        {
+            this.order = order;
+            this.templatePath = templatePath;
+
+            if (string.IsNullOrEmpty(order.born))
+                this.reader = new StreamReader(templatePath);
+            else
+                this.reader = new StreamReader(templatePath.Replace(".svg", "2.svg"));
+            this.writer = new StreamWriter(order.name + ".svg");
+            this.action = Action.None;
+        }
+
+        #region Methods
+        public void Save()
+        {
+            try
+            {
+                string line = reader.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    CheckAction(line);
+                    //writer.WriteLine(line);
+                    line = reader.ReadLine();
+                }
+
+                writer.Flush();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                throw ex;
+            }
+            finally { reader.Close(); writer.Close(); }
+        }
+        private void CheckAction(string line)
+        {
+            //"name","jawi","deathmuslim","deathmonth","death"
+            if (line.Contains("id=\"name\""))
+                this.action = Action.IsWritingName;
+            else if (line.Contains("id=\"jawi\""))
+                this.action = Action.IsWritingJawi;
+            else if (line.Contains("id=\"deathmuslim\""))
+                this.action = Action.IsWritingMuslimDeath;
+            else if (line.Contains("id=\"deathmonth\""))
+                this.action = Action.IsWritingMuslimMonth;
+            else if (line.Contains("id=\"death\""))
+                this.action = Action.IsWritingDeath;
+            else if (line.Contains("id=\"borndate\""))
+                this.action = Action.IsWritingBorn;
+
+
+            switch (this.action)
+            {
+                case Action.IsWritingName:
+                    WaitToWriteName(line);
+                    break;
+                case Action.IsWritingJawi:
+                    WaitToWriteJawi(line);
+                    break;
+                case Action.IsWritingMuslimDeath:
+                    WaitToWriteMuslimDeath(line);
+                    break;
+                case Action.IsWritingMuslimMonth:
+                    WaitToWriteMuslimMonth(line);
+                    break;
+                case Action.IsWritingDeath:
+                    WaitToWriteDeath(line);
+                    break;
+                case Action.IsWritingBorn:
+                    WaitToWriteBorn(line);
+                    break;
+                default:
+                    writer.WriteLine(line);
+                    break;
+            }
+        }
+        private void WriteElement(string value, string line)
+        {
+            int start = line.IndexOf('>');
+            if (start > -1)
+            {
+                int end = line.IndexOf('<');
+                string newLine = string.Empty;
+                newLine += line.Substring(0, start + 1);
+                newLine += value;
+                newLine += line.Substring(end, line.Length - end);
+
+                writer.WriteLine(newLine);
+                this.action = Action.None;
+            }
+            else
+                writer.WriteLine(line);
+        }
+
+        private void WaitToWriteName(string line)
+        {
+            WriteElement(order.name.ToUpper(), line);
+        }
+        private void WaitToWriteJawi(string line)
+        {
+            WriteElement(order.jawi, line);
+        }
+        private void WaitToWriteDeath(string line)
+        {
+            string[] dates = order.death.Split(new char[] { '-' });
+            string date = string.Empty;
+            if (dates.Length == 3)
+            {
+                date += Convert.ToInt32(dates[2].Substring(0, 2)).ToString() + ".";
+                date += Convert.ToInt32(dates[1].Substring(0, 2)).ToString() + ".";
+                date += dates[0];
+                //for (int i = dates.Length - 1; i >= 0; i--)
+                //    date += dates[i] + ".";
+                //date = date.TrimEnd(new char[] { '.' });
+
+                WriteElement(date, line);
+            }
+        }
+        private void WaitToWriteMuslimDeath(string line)
+        {
+            string[] dates = order.deathm.Split(new char[] { '-' });
+            string date = string.Empty;
+            if (dates.Length == 3)
+            {
+                date = dates[0] + Convert.ToInt32(dates[2].Substring(0, 2)).ToString();
+                WriteElement(date, line);
+            }
+        }
+        private void WaitToWriteMuslimMonth(string line)
+        {
+            string[] dates = order.deathm.Split(new char[] { '-' });
+            string date = string.Empty;
+            if (dates.Length == 3)
+            {
+                int month = Convert.ToInt32(dates[1]);
+                date = muslimMonths[month - 1];
+                WriteElement(date, line);
+            }
+        }
+        private void WaitToWriteBorn(string line)
+        {
+            string[] dates = order.born.Split(new char[] { '-' });
+            string date = string.Empty;
+            if (dates.Length == 3)
+            {
+                date += Convert.ToInt32(dates[2].Substring(0, 2)).ToString() + ".";
+                date += Convert.ToInt32(dates[1].Substring(0, 2)).ToString() + ".";
+                date += dates[0];
+
+                WriteElement(date, line);
+            }
+        }
+        #endregion
+
+        enum Action
+        {
+            None,
+            IsWritingName,
+            IsWritingJawi,
+            IsWritingDeath,
+            IsWritingMuslimDeath,
+            IsWritingMuslimMonth,
+            IsWritingBorn,
+        }
+    }
+}
