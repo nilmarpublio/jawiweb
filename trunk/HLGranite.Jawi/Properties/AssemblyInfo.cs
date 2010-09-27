f528feaaec1a660ddc015302ec1360c9081e12d4
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2010-09-27 02:10:34 +0000

    added separator at statusbar.

--- trunk/HLGranite.Jawi/Properties/AssemblyInfo.cs
+++ trunk/HLGranite.Jawi/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HLGranite.Jawi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RoundHeart Inc.")]
+[assembly: AssemblyCompany("HLGranite Manufacturing")]
 [assembly: AssemblyProduct("HLGranite.Jawi")]
-[assembly: AssemblyCopyright("Copyright © RoundHeart Inc. 2010")]
+[assembly: AssemblyCopyright("Copyright © HLGranite Manufacturing 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.926.185")]
-[assembly: AssemblyFileVersion("0.10.926.185")]
+[assembly: AssemblyVersion("0.10.927.192")]
+[assembly: AssemblyFileVersion("0.10.927.192")]
