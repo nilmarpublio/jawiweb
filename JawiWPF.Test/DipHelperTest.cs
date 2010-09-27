cdf19469480740afdb74515d5db439e104ca29b2
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2010-09-27 03:52:45 +0000

    (Closes issue #3) : Show ruler in drawing workspace. created and fixed some unit tests too. handled generating year value only in both born and death entry.

---
+++ JawiWPF.Test/DipHelperTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HLGranite.WPF;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace JawiWPF.Test
+{
+    /// <summary>
+    /// This is a test class for DipHelperTest and is intended
+    /// to contain all DipHelperTest Unit Tests
+    /// </summary>
+    [TestClass()]
+    public class DipHelperTest
+    {
+        public DipHelperTest()
+        {
+        }
+        /// <summary>
+        /// Test divide by 8 double value for inch units.
+        /// </summary>
+        [TestMethod()]
+        public void InchLoopingTest()
+        {
+            double[] expecteds = new double[] { 0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0 };
+            double[] actuals = new double[8];
+            int max = 8;
+            for (int i = 0; i < max; i++)
+            {
+                decimal m = 0.125m;// Convert.ToDecimal(1 / max);
+                System.Diagnostics.Debug.WriteLine(m);
+                actuals[i] = Convert.ToDouble(i * m);
+                System.Diagnostics.Debug.WriteLine(actuals[i]);
+            }
+            for (int i = 0; i < Math.Min(expecteds.Length, actuals.Length); i++)
+                Assert.AreEqual(expecteds[i], actuals[i]);
+        }
+    }
+}
--- JawiWPF.Test/Properties/AssemblyInfo.cs
+++ JawiWPF.Test/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SvnFileGenerator")]
-[assembly: AssemblyDescription("Created by yancyn@gmail.com")]
+[assembly: AssemblyTitle("JawiWPF.Test")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HLGranite Manufacturing")]
-[assembly: AssemblyProduct("SvnFileGenerator")]
-[assembly: AssemblyCopyright("Copyright © HLGranite Manufacturing 2010")]
+[assembly: AssemblyCompany("Plexus TG")]
+[assembly: AssemblyProduct("JawiWPF.Test")]
+[assembly: AssemblyCopyright("Copyright © Plexus TG 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21dab371-1d37-493e-9d82-6ebb7962dd91")]
+[assembly: Guid("a2c14b28-1ef1-403b-9116-84b184584a3e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.927.192")]
-[assembly: AssemblyFileVersion("0.10.927.192")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
