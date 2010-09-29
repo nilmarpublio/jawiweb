647182ae5d623612d09c893bbcdf23bd687bf13b
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2010-09-29 09:11:34 +0000

    (Closes issue #37) : Highlight unmatch name.

--- trunk/HLGranite.Jawi/WordCollection.cs
+++ trunk/HLGranite.Jawi/WordCollection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Controls;
 
 namespace HLGranite.Jawi
 {
@@ -22,6 +23,44 @@ namespace HLGranite.Jawi
         public new int Contains(string name)
         {
             return base.Contains(name);
+        }
+        /// <summary>
+        /// Search contains this keyword and highlight unmatch as red.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public int Contains(string name, out Dictionary<string, bool> keys)
+        {
+            //if (sender is RichTextBox)
+            //{
+            //RichTextBox textBox = (RichTextBox)sender;
+            //textBox.Document.Blocks.Clear();
+            keys = new Dictionary<string, bool>();
+            name = name.ToLower();
+            name = name.TrimEnd(new char[] { '\n', '\r' });//hack: '\n', '\r'
+            //name = TrimHiddenCharacters(name);
+            string[] names = name.Split(new char[] { ' ' });
+            foreach (string s in names)
+            {
+                if (s.Trim().Length == 0) continue;
+
+                if (this.Items.Where(f => f.Name.Contains(s)).Count() > 0)
+                    keys.Add(s, true);
+                else
+                    keys.Add(s, false);
+            }
+
+            return base.Contains(name);
+        }
+        private string TrimHiddenCharacters(string source)
+        {
+            string output = source;
+            char[] hiddens = new char[] { '\n', '\r', '\t' };
+            foreach (char hidden in hiddens)
+                //output = output.TrimStart(new char[] { hidden}).TrimEnd(new char[]{hidden});
+                output = output.Replace(hidden, '\0');
+            return output;
         }
     }
 }
