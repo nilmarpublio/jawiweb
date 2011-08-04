af72f34b893f719a294997aad4765fdee7c9ce4d
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2011-08-04 03:12:06 +0000

    removed merge button. Assume the input source will be the file name to generate otherwise will prompt to keyin again.

--- JawiWPF/Properties/AssemblyInfo.cs
+++ JawiWPF/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.409.374")]
-[assembly: AssemblyFileVersion("0.11.409.374")]
+[assembly: AssemblyVersion("0.11.804.427")]//[assembly: AssemblyVersion("0.11.409.374")]
+[assembly: AssemblyFileVersion("0.11.804.427")]
