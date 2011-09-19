17775a287b6497da1dcf2ac2f13a9b83f557f4b4
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2011-09-19 09:07:07 +0000

    fixed interpreted name wrongly for those svg file ended with 's'. allow search with empty space keyword.

--- trunk/JawiWPF/Properties/AssemblyInfo.cs
+++ trunk/JawiWPF/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.822.459")]
-[assembly: AssemblyFileVersion("1.11.822.459")]
+[assembly: AssemblyVersion("1.11.919.479")]
+[assembly: AssemblyFileVersion("1.11.919.479")]
