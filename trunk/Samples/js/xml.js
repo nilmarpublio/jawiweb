3f9d5988da8de49c8af2717e3871e68f1017c69f
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2011-10-13 03:08:10 +0000

    hooked sorting into nisan pending webpage.

--- trunk/Samples/js/xml.js
+++ trunk/Samples/js/xml.js
@@ -12,51 +12,77 @@ $(function () {
     function xml_parser(wrapper) {
         $.ajax({
             type: 'GET',
-            url: 'books.xml',
+            url: 'nisan.xml',
             dataType: 'xml',
             success: function (xml_list) {
 
                 var xmlArr = [];
-                $(xml_list).find('Book').each(function () {
-                    //property fields
-                    var xml_isbn = $(this).find('ISBN').text();
-                    var xml_title = $(this).find('Title').text();
-                    var xml_author = $(this).find('Author').text();
-                    var xml_publisher = $(this).find('Publisher').text();
-                    var xml_publishedDate = $(this).find('PublishedDate').text();
-                    var xml_page = $(this).find('TotalPage').text();
-                    var xml_price = $(this).find('Price').text();
+                $(xml_list).find('order').each(function () {
 
-                    xmlArr += '<tr>';
+                    var xml_deliver = $(this).attr('delivered');
+                    if (xml_deliver == '') {
 
-                    xmlArr += '<td>';
-                    xmlArr += xml_isbn;
-                    xmlArr += '</td>';
+                        //property fields
+                        var xml_soldto = $(this).attr('soldto');
+                        var xml_order = $(this).attr('date');
+                        var xml_price = $(this).attr('price');
 
-                    xmlArr += '<td>';
-                    xmlArr += xml_title;
-                    xmlArr += '</td>';
+                        var xml_name = $(this).find('name').text();//@todo: romanize the name letter
+                        var xml_jawi = $(this).find('jawi').text();
+                        var xml_born = $(this).find('born').text();
+                        var xml_death = $(this).find('death').text();
+                        var xml_deathm = $(this).find('deathm').text();
+                        var xml_age = $(this).find('age').text();
+                        var xml_remarks = $(this).find('remarks').text();
+                        var xml_item = $(this).find('item').text();
 
-                    xmlArr += '<td>';
-                    xmlArr += xml_page;
-                    xmlArr += '</td>';
+                        //compute desciption
+                        var desc = '';
+                        desc += xml_name;
+                        desc += ' (';
+                        if (xml_born != '') {
+                            desc += xml_born;
+                            desc += '~';
+                        }
+                        desc += xml_death;
+                        if (xml_deathm != '') {
+                            desc += '=';
+                            desc += xml_deathm;
+                        }
+                        desc += ') ';
+                        desc += xml_jawi;
 
-                    xmlArr += '<td>';
-                    xmlArr += xml_price;
-                    xmlArr += '</td>';
 
-                    xmlArr += '<td>';
-                    xmlArr += xml_publishedDate;
-                    xmlArr += '</td>';
+                        xmlArr += '<tr>';
 
-                    xmlArr += '</tr>';
+                        xmlArr += '<td>';
+                        xmlArr += '</td>';
+
+                        xmlArr += '<td>';
+                        xmlArr += xml_soldto;
+                        xmlArr += '</td>';
+
+                        xmlArr += '<td>';
+                        xmlArr += xml_order;
+                        xmlArr += '</td>';
+
+                        xmlArr += '<td>';
+                        xmlArr += desc;
+                        xmlArr += '</td>';
+
+                        xmlArr += '<td>';
+                        xmlArr += xml_item;
+                        xmlArr += '</td>';
+
+                        xmlArr += '</tr>';
+                    }
                 }); //end loop
 
                 //append array to table
                 $(xmlArr).appendTo(wrapper + ' table tbody');
 
                 //Add sort and zebra stripe to table (NOTE: this does not work as intended with sort feature)
-                window.setTimeout('$("' + wrapper + ' table").tablesorter({sortList:[[0,0],[0,0]], widgets: [\'zebra\']});', 120);
+                window.setTimeout('$("' + wrapper + ' table").tablesorter();', 120);
                 $(wrapper + ' table').hide().slideDown('200');
             }
         }); //end ajax
