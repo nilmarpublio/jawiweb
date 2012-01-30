3e4b5bf2c97cbd3c39bbf6b002cf4f87184b3bbc
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2012-01-30 06:58:21 +0000

    separate class. added unit test for convert Gregorian date to Muslim.

--- HLGranite.Jawi.Test/MuslimCalendarTest.cs
+++ HLGranite.Jawi.Test/MuslimCalendarTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Text.RegularExpressions;
+using System.Data;
 
 namespace HLGranite.Jawi.Test
 {
@@ -61,6 +62,53 @@ namespace HLGranite.Jawi.Test
             Regex regex = new Regex(@"[0-9]{4}[-][0-9]{2}[-][0-9]{2}");
             bool result = regex.IsMatch("2011-0845");
             Assert.IsFalse(result);
+        }
+        /// <summary>
+        /// A test for conversion from Gregorian date to Muslim date.
+        /// </summary>
+        [TestMethod()]
+        public void GetDateTest()
+        {
+            MuslimCalendar calendar = new MuslimCalendar(ReadXml(@"G:\projects\JawiWeb\HLGranite.Jawi\muslimcal.xml"));
+
+            //2012-01-18 is 1433-02-24
+            calendar.GetDate(new DateTime(2012, 1, 18));
+            DateTime expected = new DateTime(1433, 2, 24);
+            DateTime actual = calendar.Value;
+            Assert.AreEqual(expected, actual);
+
+            //2011-10-30 is 1432-11-03
+            calendar.GetDate(new DateTime(2011, 10, 30));
+            expected = new DateTime(1432, 11, 3);
+            actual = calendar.Value;
+            Assert.AreEqual(expected, actual);
+
+            //2011-11-27 is 1433-01-01
+            calendar.GetDate(new DateTime(2011, 11, 27));
+            expected = new DateTime(1433, 1, 1);
+            actual = calendar.Value;
+            Assert.AreEqual(expected, actual);
+        }
+        private DataTable ReadXml(string fileName)
+        {
+            DataTable table = new DataTable();
+            DataSet dataset = new DataSet();
+
+            try
+            {
+                if (System.IO.File.Exists(fileName))
+                    dataset.ReadXml(fileName);
+                if (dataset.Tables.Count > 0)
+                    table = dataset.Tables[0].Copy();
+
+                return table;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                return table;
+            }
+            finally { dataset.Dispose(); }
         }
     }
 }
