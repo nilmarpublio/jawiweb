e5b2b1ec7d9e868a0eac0764075f5435c290e5bf
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2012-03-07 06:36:40 +0000

    added jawi translate from ejawi.net method.

---
+++ HLGranite.Jawi.Test/JawiTranslatorTest.cs
@@ -0,0 +1,27 @@
+﻿using HLGranite.Jawi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HLGranite.Jawi.Test
+{
+    /// <summary>
+    /// This is a test class for JawiTranslatorTest and is intended
+    /// to contain all JawiTranslatorTest Unit Tests
+    /// </summary>
+    [TestClass()]
+    public class JawiTranslatorTest
+    {
+        /// <summary>
+        ///A test for Translate
+        ///</summary>
+        [TestMethod()]
+        public void TranslateTest()
+        {
+            JawiTranslator target = new JawiTranslator();
+            string rumi = "pagi";
+            string expected = "ڤاݢي";
+            string actual = target.Translate(rumi);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
