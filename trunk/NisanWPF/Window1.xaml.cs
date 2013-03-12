23e057b93168c806abe7419d78b5f2e2f412589d
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2013-03-12 01:48:25 +0000

    Initial interface for NisanWPF project which is an app for nisan entry maintenance. Draft basic interface with ribbon control style. Added useful icons.

---
+++ trunk/NisanWPF/Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yeang-shing.then
+ * Date: 3/5/2013
+ * Time: 5:27 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace NisanWPF
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
