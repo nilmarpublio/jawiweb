55116c6a89f5bedf8e344cef051b0c9c3db2293b
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2013-05-29 12:27:14 +0000

    Download Malay dictionary for retrieve jawi later use.

---
+++ DownloadJawi/GetJawiDictionary.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yeang-shing.then
+ * Date: 5/29/2013
+ * Time: 5:41 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace DownloadJawi
+{
+	/// <summary>
+	/// Description of GetJawiDictionary.
+	/// </summary>
+	[TestFixture]
+	public class GetJawiDictionary
+	{
+		[Test]
+		public void Execute()
+		{
+		}
+	}
+}
