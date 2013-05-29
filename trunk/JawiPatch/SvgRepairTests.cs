6e150d66f5855549a08458938575b9c0b77da173
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2013-05-29 09:45:17 +0000

    Initial NisanWPF for nisan entry.

---
+++ trunk/JawiPatch/SvgRepairTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yeang-shing.then
+ * Date: 11/16/2012
+ * Time: 1:32 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace JawiPatch
+{
+	/// <summary>
+  /// Description of SvgRepairTest.
+  /// </summary>
+  [TestFixture]
+  public class SvgRepairTests
+  {
+    [Test]
+    public void PatchLineTest()
+    {
+      SvgRepair repair = new SvgRepair("D:\\Output");
+      string target = "sodipodi:nodetypes=\"ccccccccccccccccccccccccccccssscsssssccscccccssssscssssccsssscsssssccccscsccssccccccccssccccsscscsscscsscccscssccsccssssssccssssssssccc\"";
+      string expected = "";
+      string actual = repair.PatchLine(target);
+      Assert.AreEqual(expected,actual);
+      
+      target = "sodipodi:linespacing=\"125%\"";
+      actual = repair.PatchLine(target);
+      Assert.AreEqual(expected,actual);
+      
+      target = "inkscape:connector-curvature=\"0\"";
+      actual = repair.PatchLine(target);
+      Assert.AreEqual(expected,actual);
+    }
+  }
+}
