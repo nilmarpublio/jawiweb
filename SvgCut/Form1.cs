db27a064f0e06cfe4ee0c1793ee8cdca2b0c7b6c
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-14 08:38:00 +0000

    Added open file dialog.

--- SvgCut/Form1.cs
+++ SvgCut/Form1.cs
@@ -15,5 +15,15 @@ namespace SvgCut
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //open up file dialog
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".svg";
+            dialog.Filter = "Scalable Vector Graphic (.svg)|*.svg";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                textBox1.Text = dialog.FileName;
+        }
     }
 }
