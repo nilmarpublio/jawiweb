4725dbccf4e8d4d88e739c7d5a969bdae4e69bf2
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-14 08:38:00 +0000

    Added open file dialog.

--- trunk/SvgCut/Form1.Designer.cs
+++ trunk/SvgCut/Form1.Designer.cs
@@ -196,6 +196,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Browse";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label7
             // 
