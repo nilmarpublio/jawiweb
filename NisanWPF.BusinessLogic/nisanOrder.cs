f59ccb12f740d7c7d4548faf702621556931ce66
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-19 10:38:11 +0000

    Bind soldto customer into list.

--- NisanWPF.BusinessLogic/nisanOrder.cs
+++ NisanWPF.BusinessLogic/nisanOrder.cs
@@ -8,6 +8,28 @@ namespace NisanWPF.BusinessLogic
 {
     public partial class nisanOrder
     {
+        /// <summary>
+        /// Gets abbreviation of soldto customer.
+        /// </summary>
+        /// <remarks>
+        /// TODO: Move abbrev setting to configuration.
+        /// </remarks>
+        public string abbrev
+        {
+            get
+            {
+                switch (this.soldto)
+                {
+                    case "ADI": return "A";
+                    case "HAM": return "H";
+                    case "KEN": return "K";
+                    case "PAS": return "P";
+                    case "SEM": return "M";
+                    case "SEL": return "S";
+                    default: return this.soldto;
+                }
+            }
+        }
 
     }
 }
