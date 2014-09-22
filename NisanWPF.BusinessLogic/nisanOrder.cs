b5aba838959c61653017c018d8e0fe3250b8001e
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-22 04:02:19 +0000

    Define soldto rectangle and color.

--- NisanWPF.BusinessLogic/nisanOrder.cs
+++ NisanWPF.BusinessLogic/nisanOrder.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Media;
 
 namespace NisanWPF.BusinessLogic
 {
     public partial class nisanOrder
     {
+        private BrushConverter brushConverter = new BrushConverter();
+
         /// <summary>
         /// Gets abbreviation of soldto customer.
         /// </summary>
@@ -26,7 +29,24 @@ namespace NisanWPF.BusinessLogic
                     case "PAS": return "P";
                     case "SEM": return "M";
                     case "SEL": return "S";
-                    default: return this.soldto;
+                    default: return this.soldto.Substring(0, 1);
+                }
+            }
+        }
+
+        public System.Windows.Media.Brush soldtoColor
+        {
+            get
+            {
+                switch (this.soldto)
+                {
+                    case "ADI": return (Brush)brushConverter.ConvertFromString("#008080");
+                    case "HAM": return (Brush)brushConverter.ConvertFromString("#00ff00");
+                    case "KEN": return Brushes.Khaki;
+                    case "PAS": return Brushes.PaleVioletRed;
+                    case "SEM": return (Brush)brushConverter.ConvertFromString("#ff5555");
+                    case "SEL": return (Brush)brushConverter.ConvertFromString("#808000");
+                    default: return Brushes.Gray;
                 }
             }
         }
