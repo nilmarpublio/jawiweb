c4c5f15ae883cc403d4bbcf838d45c8900f9e423
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-23 06:15:22 +0000

    Added hasCut property to nisanOrder to bind the checkbox.

--- trunk/NisanWPF.BusinessLogic/nisanOrder.cs
+++ trunk/NisanWPF.BusinessLogic/nisanOrder.cs
@@ -31,8 +31,41 @@ namespace NisanWPF.BusinessLogic
         {
             get
             {
-                if (this.delivered == null) return false;
+                if (this.delivered == null)
+                {
+                    this.hasCutField = false;
+                    return false;
+                }
+                else
+                {
+                    if (this.delivered.Length > 0)
+                        this.hasCutField = true;
+                    else
+                        this.hasCutField = false;
+                }
+
                 return (this.delivered.Length > 0);
+            }
+        }
+
+        private bool hasCutField;
+        /// <summary>
+        /// Gets or sets this order has plotted the sticker work.
+        /// </summary>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool hasCut
+        {
+            get
+            {
+                return this.hasCutField;
+            }
+            set
+            {
+                if ((hasCutField.Equals(value) != true))
+                {
+                    this.hasCutField = value;
+                    this.OnPropertyChanged("hasCut");
+                }
             }
         }
 
