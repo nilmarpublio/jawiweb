13da77c74af812b62c0c530f33ab6c8483ab9b51
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-25 02:28:06 +0000

    Not to show knife icon if already delivered. Reluctant.

--- NisanWPF/Resources/Converters.cs
+++ NisanWPF/Resources/Converters.cs
@@ -54,6 +54,55 @@ namespace NisanWPF
         }
         #endregion
     }
+    /// <summary>
+    /// If true then invisible otherwise visible.
+    /// </summary>
+    /// <remarks>
+    /// Opposite with VisibilityConverter.
+    /// </remarks>
+    public class InvisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value is Boolean)
+            {
+                return (Boolean)value ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value is int)
+            {
+                return ((int)value) > 0 ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value is Int32)
+            {
+                return System.Convert.ToInt32(value) > 0 ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value is decimal)
+            {
+                return System.Convert.ToDecimal(value) > 0 ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value is float)
+            {
+                float money = (float)value;
+                return (money > 0) ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            throw new ArgumentException("Not supported type of " + value.GetType());
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
 
     /// <summary>
     /// Gets abbreviation of soldto customer.
