63aecd17fedd43be611962e1b4495e52dd3fb2e7
yancyn <yancyn@1ff27ab0-a4d0-11dd-91bf-632da8b6e10b>
2014-09-25 06:38:22 +0000

    Added namesapce to HLGranite.Jawi.nisanOrder.

--- SvgFileGenerator/Properties/AssemblyInfo.cs
+++ SvgFileGenerator/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.12.1020.723")]
-[assembly: AssemblyFileVersion("1.12.1020.723")]
+[assembly: AssemblyVersion("1.14.925.1030")]
+[assembly: AssemblyFileVersion("1.14.925.1030")]
